20ed822064ef925332a820c596adef864d29a525
Humberto  Villegas <109755324+villegasleon@users.noreply.github.com>
2023-09-18 14:10:37 -0600

    Add files via upload

---
+++ token_.py
@@ -0,0 +1,6 @@
+token = 'AquivaTuTokenObtenidoEn_https://iexcloud.io/'
+# Tu dirección de correo electrónico y contraseña
+email_address = 'correoDeDondeEnviaras@gmail.com'
+#Deberas activar la opcion emailpassword para apis, asi no expondras directamente tu password en el codigo
+#INgresa en gmail autenticacion 2fa y hasta abajo viene esa opcion de apis
+email_password = 'ActivaEmailPasswordenGmAil'
